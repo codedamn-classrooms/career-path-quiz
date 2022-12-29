fde1a2bbb0827d5a02b9ad741a8b9a3088395b8f
Pranav <pranav.techiegeek@gmail.com>
2022-12-29 12:56:19 +0530

    basic lab done

---
+++ script.js
@@ -0,0 +1,10 @@
+const str = "JavaScript is awesome";
+
+function reverseAString(str) {
+  // write your solution here
+
+  // return your reversed string here
+  return;
+}
+
+console.log(`Reversed string is: ${reverseAString(str)}`);
